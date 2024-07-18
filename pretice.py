a6daf158b921677792e7c609db1e8384403f10b8
Abdul Basit <120900763+iam-AbdulBasit@users.noreply.github.com>
2024-07-18 16:34:02 +0500

    Add files via upload

---
+++ pretice.py
@@ -0,0 +1,50 @@
+# list = [4,1,2,3,11,5,6,7,8]
+# print(type(list))
+# list.append(10)
+# print(list)
+# list.sort()
+# list.sort(reverse=True)
+# list.reverse()
+# print(list.index(1))
+# print(list.count(1))
+# new = list.copy()
+# list.insert(2, 114)
+# l = [11,22,33,44,55]
+# list.extend(l)
+# if list is sorted:
+#     print("list is sorted")
+# else:
+#     print("list is not sorted")    
+# print(list)
+
+# list = ["lahore", "karachi", "kotli", "bhimber", "rawalakot"]
+
+# city = input("enter the name of any city in pakistan: ")
+
+# if city in list:
+#     print("cleanest")
+# else:
+    # print("dirty city")
+
+# list = ["kotli", "bagh", "lahore", "multan", "islamabad"]
+
+# city = input("enter the name of any city:")
+
+# if city in list:
+#     print("cleanest city")
+# else:
+#     print("dirty city")    
+
+# num = int(input("how many number you want to enter: "))
+# list = []
+
+# for i in range(num):
+#      number = int(input(f"enter number  {i + 1}: "))
+#      list.append(number)
+# print(list)   
+
+
+#task
+num = int(input("enter the table of number: "))
+for i in range(1,11):
+    print(f"{num} x {i} = {num*i}")  
